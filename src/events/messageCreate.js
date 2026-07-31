592003e4b6180add88866f22be61859638bdbf0c
nosi <nosi@test.test>
2026-07-31 23:37:49 +0200

    feat: move bump recall logic to dedicated event module

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -151,29 +151,7 @@ module.exports = {
 
     async execute(message) {
         // Ignorer les messages du bot lui-même
-        if (message.author.bot) {
-            if (message.author.id == '302050872383242240') { // Disboard Bot
-                if (message.embeds.length === 0) return;
-                const chaineRecherchee = "Bump effectué !"
-                for (const embed of message.embeds) {
-                    if (embed.description && embed.description.includes(chaineRecherchee)) {
-
-                        const guild = await message.guild;
-                        //const channel = await guild.channels.fetch(CHANNEL_ID);
-
-                        const date = new Date();
-                        date.setHours(date.getHours() + 2);
-                        const heureParis = date.toLocaleString('fr-FR', { timeZone: 'Europe/Paris' });
-                        await console.log(`[BUMP] Bump effectué, Prochain Bump à : ${heureParis}`)
-                        setTimeout(async () => {
-                            await message.channel.send(`<@&1427703047534153872> **c'est l'heure de bumper Obsydian** <:Obsydemoncouverture:1488145689916473544>`); // 1488145689916473544
-                            const heureParis = new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' });
-                            await console.log(`[BUMP] 2 heures se sont écoulées, le rappel a été envoyé à ${heureParis}!`)
-                        }, 2 * 60 * 60 * 1000); // Deux heures
-                    }
-                }
-            }
-        } else {
+        if (!message.author.bot) {
             //return false; //to deactivate the captcha
             // Vérifier si c'est une réponse au captcha
             const isCaptchaResponse = await handleCaptchaResponse(message);
